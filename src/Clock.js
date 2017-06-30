1e59acfcf850317788f678305a846fdcd742f2d6
Spencer Gerdes <skgerdes@yahoo.com>
2017-06-29 21:33:57 -0500

    Stored date in state

--- src/Clock.js
+++ src/Clock.js
@@ -2,9 +2,15 @@ import React, { Component } from 'react';
 
 class Clock extends Component
 {
+	constructor(props)
+	{
+		super(props);
+		this.state = {date: new Date()}
+	}
+
 	render()
 	{
-		return <p>The time is currently {new Date().toLocaleTimeString()}</p>
+		return <p>The time is currently { this.state.date.toLocaleTimeString() }</p>
 	}
 }
 
