56cb47d322b7c22d7aff0c901ea8f21e272d4809
Spencer Gerdes <skgerdes@yahoo.com>
2017-06-29 21:37:41 -0500

    Uses lifecycle hooks to update the state

--- src/Clock.js
+++ src/Clock.js
@@ -12,6 +12,21 @@ class Clock extends Component
 	{
 		return <p>The time is currently { this.state.date.toLocaleTimeString() }</p>
 	}
+
+	componentDidMount()
+	{
+		this.timer = setInterval(() => this.tick(), 1000);
+	}
+
+	componentWillUnmount()
+	{
+		clearInterval(this.timer);
+	}
+
+	tick()
+	{
+		this.setState({date: new Date()})
+	}
 }
 
 export default Clock;
